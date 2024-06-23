2e5f17f16c9e471c2fa19a107d5b60b1c73d6cbb
Alex Chinedu <nwekealexchinedu@gmail.com>
2024-06-23 14:49:30 +0100

    wrote some more test for the sort page function.

--- src/report.test.ts
+++ src/report.test.ts
@@ -1,13 +1,32 @@
 import { test, expect } from "@jest/globals";
 import { sortPages } from "./report";
 
-test("sortPage", () => {
+test("sortPage 2 pages", () => {
   const input = {
     "https://blog.dev.tov/path": 3,
     "https://blog.dev.tov": 4,
   };
   const actual = sortPages(input);
   const expected = [
+    ["https://blog.dev.tov", 4],
+    ["https://blog.dev.tov/path", 3],
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("sortPage 5 pages", () => {
+  const input = {
+    "https://blog.dev.tov/path": 3,
+    "https://blog.dev.tov": 4,
+    "https://blog.dev.tov/path1": 9,
+    "https://blog.dev.tov/path2": 7,
+    "https://blog.dev.tov/path3": 42,
+  };
+  const actual = sortPages(input);
+  const expected = [
+    ["https://blog.dev.tov/path3", 42],
+    ["https://blog.dev.tov/path1", 9],
+    ["https://blog.dev.tov/path2", 7],
     ["https://blog.dev.tov", 4],
     ["https://blog.dev.tov/path", 3],
   ];
