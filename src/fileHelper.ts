1314b4b37b37a0d760b363d19e037c520977efee
Alex Chinedu <nwekealexchinedu@gmail.com>
2024-06-26 20:12:52 +0100

    create ensure file exists utile function.

---
+++ src/fileHelper.ts
@@ -0,0 +1,15 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+
+export async function ensureFileExists(filePath: string) {
+  try {
+    await fs.access(filePath);
+  } catch (error: any) {
+    // If the file does not exist, create it
+    if (error?.code === "ENOENT") {
+      await fs.writeFile(filePath, "");
+    } else {
+      throw error;
+    }
+  }
+}
